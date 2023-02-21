7cf6c9b21e55eafc2fbdd254a61adfbb2fdde770
João <joaoaugustopfpf@gmail.com>
2023-02-21 10:52:37 -0300

    updating confirm email v2

--- src/pages/components/Confirm-Email/index.js
+++ src/pages/components/Confirm-Email/index.js
@@ -1,39 +1,7 @@
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { FormLoading } from "../../../lib/Form";
-
 export default function ConfirmCode({ query }) {
-	const router = useRouter();
-	const [success, setSuccess] = useState(false);
-	const [error, setError] = useState(false);
-
-	const handleConfirmation = async () => {
-		if (query.confirmation) {
-			setSuccess(true);
-			await axios
-				.get(
-					`${process.env.NEXT_PUBLIC_API_URL}/api/auth/email-confirmation?confirmation=${query.confirmation}`
-				)
-				.then(() => {
-					setSuccess(true);
-					router.push("/login");
-				})
-				.catch(() => {
-					console.log(error);
-					return;
-				});
-		}
-	};
-	handleConfirmation();
-	useEffect(() => {
-		if (!query.confirmation) {
-			router.push("/");
-		}
-	}, []);
 	return (
 		<>
-			{success ? (
+			{query ? (
 				<>
 					<div>
 						<h4 className="text-success">E-mail confirmado!</h4>
--- src/pages/confirme-codigo.js
+++ src/pages/confirme-codigo.js
@@ -1,5 +1,6 @@
 import Auth from "../templates/Auth";
 import ConfirmEmail from "../pages/components/Confirm-Email/index";
+import axios from "axios";
 
 export default function ConfirmCode({ query }) {
 	return (
@@ -13,8 +14,27 @@ export default function ConfirmCode({ query }) {
 
 export async function getServerSideProps(context) {
 	const query = context.query;
+	if (!context.query.confirmation) {
+		context.res.setHeader("Location", "/");
+		context.res.statusCode = 302;
+		return;
+	}
 
-	return {
-		props: { query }
-	};
+	if (query.confirmation) {
+		await axios
+			.get(
+				`${process.env.NEXT_PUBLIC_API_URL}/api/auth/email-confirmation?confirmation=${query.confirmation}`
+			)
+			.then(() => {
+				return {
+					props: { query: true }
+				};
+			})
+			.catch((error) => {
+				console.log(error);
+				return {
+					props: { query: false }
+				};
+			});
+	}
 }
